d85ddba4a42576cce702b9dfd8744441cb6df40e
Carlos Garay <garaycarlosalfredo@gmail.com>
2021-11-28 14:23:57 -0300

    [BACK][Agrega punto formulario con colores]

--- js/arcGis.js
+++ js/arcGis.js
@@ -65,7 +65,7 @@ var map
         //Create Map View
         const view = new MapView({
             map: map,
-            center: [-118.805, 34.027], // Longitude, latitude
+            center: [-58.435698,-34.606941], // Longitude, latitude
             zoom: 13, // Zoom level
             container: "viewDiv" // Div element
         });
@@ -87,8 +87,8 @@ var map
         //Para agregar un punto 
         const point = { //Create a point
             type: "point",
-            longitude: -118.80657463861,
-            latitude: 34.0005930608889
+            longitude: -34.590924,
+            latitude: -58.459808
         };
         const simpleMarkerSymbol = {
             type: "simple-marker",
@@ -112,15 +112,30 @@ var map
     }
 
     function checkPint(e){   
-        console.log(e)
+       /* console.log(e)
         const form = document.getElementById("formulario")
-        console.log(form)
+        //console.log(form)
 
-        const nombre = form.elements['nombre'].value
-        console.log(nombre)
+        const coordenadasForm = form.elements['coordenadas'].value
+        const coordenadas = coordenadasForm.split(',')
+        console.log(coordenadas)*/
     }
 
   function addPoint(){
+
+    //Obtengo los datos del formulario
+    const form = document.getElementById("formulario")
+    const nombre = form.elements['nombre'].value
+    const direccion = form.elements['direccion'].value
+    const telefono = form.elements['telefono'].value
+    const categoria = form.elements['categoria'].value
+    const coordenadasForm = form.elements['coordenadas'].value
+    const colorForm = form.elements['color'].value
+
+    //Gestion de coordenadas
+    const coordenadas = coordenadasForm.split(',')
+    const latitude = coordenadas[0]
+    const longitude = coordenadas[1]
    
     require([
        "esri/config",
@@ -132,8 +147,9 @@ var map
        "esri/layers/TileLayer",
        //Para agregar un punto
        "esri/Graphic",
-       "esri/layers/GraphicsLayer"
-       
+       "esri/layers/GraphicsLayer",
+       //PopUp
+       "esri/popup/content/FieldsContent"
 
    ], function (
        esriConfig,
@@ -145,24 +161,27 @@ var map
        TileLayer,
        //Para agregar un punto
        Graphic, 
-       GraphicsLayer
+       GraphicsLayer,
+       //Popup
+       FieldsContent
        ) {
 
    esriConfig.apiKey = key;
 
 
-  
-
    //Pop up
    const popupTemplate = {
        title: "{Name}",
-       content: "{Description}"
+       content: `<b>Nombre :</b> {Direccion}<br><b>Coordenadas :</b> {Coordenadas}<br><b>Telefono:</b> {Telefono}<br><b>Categoría:</b> {Categoria}<br>`,
    }
    const attributes = {
-       Name: "Graphic",
-       Description: "I am a polygon"
-   }
-
+        Name: nombre,
+        Direccion: direccion,
+        Coordenadas: coordenadas,
+        Telefono : telefono,
+        Categoria : categoria
+    }
+ 
    //Para agregar un punto
    const graphicsLayer = new GraphicsLayer();
    map.add(graphicsLayer);
@@ -170,12 +189,21 @@ var map
    //Para agregar un punto 
    const point = { //Create a point
        type: "point",
-       longitude: -118.80657463861,
-       latitude: 34.0005930608889
+       longitude: longitude,
+       latitude: latitude
    };
+
+   var color = [211, 214, 19]
+   if(colorForm === 'Azul') color = [18, 16, 158]
+   if(colorForm === 'Rojo') color = [181, 32, 9]
+   if(colorForm === 'Verde') color = [23, 87, 12]
+
+   console.log(color)
+
+
    const simpleMarkerSymbol = {
        type: "simple-marker",
-       color: [226, 119, 40],  // Orange
+       color: color,  // Orange
        outline: {
            color: [255, 255, 255], // White
            width: 1
@@ -189,12 +217,8 @@ var map
        attributes: attributes,
        popupTemplate: popupTemplate
    });
+
    graphicsLayer.add(pointGraphic);
-
-
-
-   
-
  });
 }
 /*
